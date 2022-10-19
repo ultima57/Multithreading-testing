fe2326a9348c7a99738436655daf30ce00e71214
ultima57 <artyomzar83@gmail.com>
2022-10-19 09:54:29 +0300

    Start

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ss");
+        System.out.println("sssssssssssssss");
+        System.out.println("new");
+    }
+}
